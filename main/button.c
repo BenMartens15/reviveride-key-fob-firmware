790f1455a90d9a2b265fc5250cb877d6c65bdd5f
Ben Martens <2benmartens15@gmail.com>
2023-10-11 22:14:05 -0400

    add deep sleep functionality

--- main/button.c
+++ main/button.c
@@ -2,8 +2,10 @@
 /* INCLUDES *******************************************************************/
 #include "button.h"
 #include "driver/gpio.h"
+#include "driver/rtc_io.h"
 #include "esp_log.h"
 #include "esp_now_core.h"
+#include "esp_sleep.h"
 #include "esp_timer.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/semphr.h"
@@ -18,7 +20,7 @@
 
 #define ESP_INTR_FLAG_DEFAULT 0
 
-#define BUTTON_PIN 27
+#define BUTTON_PIN 15
 #define BUTTON_PIN_MASK (1 << BUTTON_PIN)
 #define BUTTON_DEBOUNCE_MS 80
 
@@ -57,25 +59,38 @@ void button_init(void)
     gpio_config_t pin_config = {
         .pin_bit_mask = BUTTON_PIN_MASK,
         .mode = GPIO_MODE_INPUT,
-        .pull_up_en = 1,
-        .intr_type = GPIO_INTR_ANYEDGE
+        .pull_up_en = 0,
+        .intr_type = GPIO_INTR_NEGEDGE
     };
 
-    gpio_config(&pin_config);
+    ESP_ERROR_CHECK(esp_sleep_enable_ext1_wakeup(BUTTON_PIN_MASK, ESP_EXT1_WAKEUP_ANY_HIGH));
 
-    button_semaphore = xSemaphoreCreateBinary();
-    xTaskCreate(button_task, "button_task", 2048, NULL, 10, NULL);
+    ESP_LOGI(TAG, "Wakeup cause: %d", esp_sleep_get_wakeup_cause());
 
-    // the purpose of the button time is to turn the car on or off as soon as the button is held for more than 2 seconds
-    button_timer = xTimerCreate("button_hold_timer",
-                    pdMS_TO_TICKS(2000),
-                    pdFALSE,
-                    (void*)0,
-                    toggleEngineState);
-    // add a check for if button_timer is NULL
+    if (gpio_get_level(BUTTON_PIN) == 1) { // if the button is still pressed
+        ESP_LOGI(TAG, "Still pressed");
 
-    gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
-    gpio_isr_handler_add(BUTTON_PIN, button_isr_handler, NULL);
+        gpio_config(&pin_config);
+
+        button_semaphore = xSemaphoreCreateBinary();
+        xTaskCreate(button_task, "button_task", 2048, NULL, 10, NULL);
+
+        // the purpose of the button time is to turn the car on or off as soon as the button is held for more than 1 seconds (plus ESP boot time)
+        button_timer = xTimerCreate("button_hold_timer",
+                        pdMS_TO_TICKS(1000),
+                        pdFALSE,
+                        (void*)0,
+                        toggleEngineState);
+        // add a check for if button_timer is NULL
+        xTimerStart(button_timer, 0); // start the 2 second button timer
+
+        gpio_install_isr_service(ESP_INTR_FLAG_DEFAULT);
+        gpio_isr_handler_add(BUTTON_PIN, button_isr_handler, NULL);
+    } else { // if the button is already released, just go back to sleep
+        ESP_LOGI(TAG, "Going to sleep");
+        esp_deep_sleep_start();
+    }
+    
 }
 /******************************************************************************/ 
 
@@ -100,17 +115,19 @@ static void button_task(void* arg)
         }
 
         if ((button_history & 0b0000000000001111) == 0b0000000000000000) {
-            ESP_LOGI(TAG, "Button pressed");
-            xTimerStart(button_timer, 0); // start the 2 second button timer
-        } else if ((button_history & 0b0000000000001111) == 0b0000000000001111) {
             ESP_LOGI(TAG, "Button released");
             xTimerStop(button_timer, 0);
+            ESP_LOGI(TAG, "Going to sleep");
+            esp_deep_sleep_start(); // go back to sleep if the button is released before 1 second
         }
     }
 }
 
 static void toggleEngineState(TimerHandle_t xTimer)
 {
+    ESP_LOGI(TAG, "Button held for 1 second");
+    gpio_intr_disable(BUTTON_PIN); // disable interrupts on the button pin
+
     engine_state = engine_state ^ 1;
     send_data_t send_data = {
         .start_stop_command = engine_state
@@ -124,5 +141,7 @@ static void toggleEngineState(TimerHandle_t xTimer)
         led_off();
         vTaskDelay(pdMS_TO_TICKS(1000));
     }
+    ESP_LOGI(TAG, "Going to sleep");
+    esp_deep_sleep_start(); // go back to sleep when finished turning the car on or off
 }
 /******************************************************************************/
